bec424649887276d65e2924de663988cee491ffc
Eudneygabriel <eudneygabriel@epfundao.edu.pt>
2025-06-02 17:21:56 +0100

    continuaçao

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import Header from "./components/Header"; // ✅ Importação adicionada
 import backgroundImage from "./assets/imagem.jpg";
 import "./index.css";
+import javaLogo from "./assets/java.png";
+import pythonLogo from "./assets/Python.png";
+import cppLogo from "./assets/c,c++.png";
+import reactLogo from "./assets/React.png";
+import jsLogo from "./assets/Javascript.png";
+import htmlLogo from "./assets/html.png";
+import vercelLogo from "./assets/vercel.png";
+import githubLogo from "./assets/github.png";
 
 export default function App() {
   return (
@@ -27,12 +35,38 @@ export default function App() {
             </p>
             <h2>Linguagens de programação</h2>
             <ul className="skills-list">
-              <li>Java</li>
-              <li>Python</li>
-              <li>C / C++</li>
-              <li>React</li>
-              <li>JavaScript</li>
+              <li>
+                <img src={javaLogo} alt="Java" className="skill-icon" />
+              </li>
+              <li>
+                <img src={pythonLogo} alt="Python" className="skill-icon" />
+              </li>
+              <li>
+                <img src={cppLogo} alt="C/C++" className="skill-icon" />
+              </li>
+              <li>
+                <img src={reactLogo} alt="React" className="skill-icon" />
+              </li>
+              <li>
+                <img src={jsLogo} alt="JavaScript" className="skill-icon" />
+              </li>
+              <li>
+                <img src={htmlLogo} alt="html" className="skill-icon" />
+              </li>
             </ul>
+
+            <section>
+              <h2>Plataformas de Deploy</h2>
+              <ul className="skills-list">
+                <li>
+                  <img src={vercelLogo} alt="Vercel" className="deploy-icon" />
+                </li>
+
+                <li>
+                  <img src={githubLogo} alt="github" className="deploy-icon" />
+                </li>
+              </ul>
+            </section>
           </div>
         </main>
       </div>
